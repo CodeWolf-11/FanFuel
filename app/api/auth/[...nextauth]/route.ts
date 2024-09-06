69e849a4ca5906b4cbc81535169c0001771733c3
Nishant Rai <n.rai.code@outlook.com>
2024-09-06 17:26:52 +0530

    deployement bug fix 1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ import NextAuth from "next-auth/next";
 import { nextAuthOptions } from "@/lib/nextAuthOptions";
 
 
-export const authOptions = NextAuth(nextAuthOptions);
+const authOptions = NextAuth(nextAuthOptions);
 
 export { authOptions as GET, authOptions as POST };
